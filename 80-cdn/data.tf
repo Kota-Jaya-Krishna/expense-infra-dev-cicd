6cc0542c556c07361938800a3d526c083d8a2921
Kota-Jaya-Krishna <jayakrishnakota23@gmail.com>
2025-04-21 00:31:07 +0530

    cicd

--- 80-cdn/data.tf
+++ 80-cdn/data.tf
@@ -6,6 +6,6 @@ data "aws_cloudfront_cache_policy" "cacheEnable" {
   name = "Managed-CachingOptimized" 
 }
 
-data "aws_ssm_parameter" "https_certificate_arn" {
-  name = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
+data "aws_ssm_parameter" "ingress_alb_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/ingress_alb_certificate_arn"
 }
